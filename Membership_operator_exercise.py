9e664a10fa7bceb5629f64b304fd575e749251f4
Harshitha2235600 <harshithapasupuleti2207@gmail.com>
2026-09-04 14:23:19 +0530

    Membership_operator_exercise.py

---
+++ Membership_operator_exercise.py
@@ -0,0 +1,9 @@
+a=[1,2,3]
+b='student'
+result=b in a # in membership operator exercise     
+print("result of",b,"in",a,"in",result) 
+
+a=[1,2,3]
+b='student'
+result=b not in a # notin membership operator exercise
+print("result of",b,"not in",a,"not in",result)
